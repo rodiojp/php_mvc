595da39ac61cad7fb256e09e0baa1e664208dd15
rodiojp <radzioshkin@gmail.com>
2018-01-09 00:07:31 -0500

    array

---
+++ Core/Router.php
@@ -0,0 +1,29 @@
+<?php
+
+class Router {
+    /*
+     *   Associative array of routes (the routing table)
+     *   @var array
+    */
+
+    protected $routes = array();
+
+    /* 
+    * Add a route to the routing table
+    * @param string $route The route URL
+    * @param array $params Parameters (Controller, action, ets.)
+    * @return void
+    */
+    public function add($route, $params)
+    {
+        $this->$routes[$route] = $params;
+    }
+    /* 
+    * Get all the routes from the routing table
+    * @return array
+    */
+    public function getRoutes()
+    {
+        return $this->$routes;
+    }
+}
--- public/index.php
+++ public/index.php
@@ -3,4 +3,23 @@
 Front Controller
 PHP 5.4
 */
-echo "Redirected URL = '" . $_SERVER["QUERY_STRING"] . "'";
+
+/*
+Routing
+*/
+require "../Core/Router.php";
+//echo "Redirected URL = '" . $_SERVER["QUERY_STRING"] . "'";
+
+$router  = new Router();
+// Add the routes
+$router->add("",["controller" => "Home", "action" => "index"]);
+$router->add("posts",["controller" => "Posts", "action" => "index"]);
+$router->add("posts/new",["controller" => "Posts", "action" => "new"]);
+
+// Dispay the routing table
+echo "<pre>";
+var_dump($router->getRoutes());
+echo "</pre>";
+
+//echo get_class($router);
+
